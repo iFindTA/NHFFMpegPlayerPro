a18ea5becf0f7bebb07fa080d4a6b9105d1a17fa
hujiaju <nanhujiaju@gmail.com>
2016-04-18 12:03:31 +0800

    init pod setting

---
+++ Podfile
@@ -0,0 +1,6 @@
+target "NHFFMpegPlayerPro" do
+
+platform:ios, '7.0'
+pod 'PBToolKits', '~>0.4.172'
+pod 'AFNetworking', '~>3.1.0'
+end
---
+++ Pods/Target Support Files/Pods-NHFFMpegPlayerPro/Pods-NHFFMpegPlayerPro-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_NHFFMpegPlayerPro : NSObject
+@end
+@implementation PodsDummy_Pods_NHFFMpegPlayerPro
+@end
